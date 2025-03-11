00017d241b22eb847c8568840e53bdcb0dca1e2d
Itati26 <ita26rey04@gmail.com>
2025-03-10 19:26:41 -0600

    Add files via upload

---
+++ Ciclos while.py
@@ -0,0 +1,6 @@
+x=int(input("Dime que tabla de multiplicar deseas visualizar"))
+a=1;
+while a <= 10: #ciclo
+    print(f"{x} * {a} = {x*a}")
+    a+=1
+    
---
+++ Decisiones Anidadas.py
@@ -0,0 +1,11 @@
+n1=int(input("Ingresa tu calificacion---->"))
+if n1==100:
+    print ("Excelente felicidades")
+elif n1<=99 and n1>=90:
+    print ("MUY BIEN")
+elif n1<=89 and n1>80:
+    print ("BIEN")
+elif n1<=79 and n1>=70:
+    print ("ALUMNO REGULAR")
+else:
+    print ("ALUMNO NO APROBADO")
